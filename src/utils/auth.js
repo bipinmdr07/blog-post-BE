b7fe2a604eb7c4680986a291eb5575b7dd932862
Bipin Manandhar <kubipin07@gmail.com>
2022-05-14 15:11:53 +0545

    Add util for extracting token from header.

---
+++ src/utils/auth.js
@@ -0,0 +1,18 @@
+import * as jwtUtils from './jwt';
+
+export function extractTokenFromHeaders(headers = {}) {
+  const { authorization = '' } = headers;
+
+  const [tokenType, token] = authorization.split(' ').filter(Boolean);
+
+  if (tokenType !== 'Bearer' || !token) {
+    throw new TokenError('Invalid Token');
+  }
+
+  jwtUtils.verifyToken(token);
+
+  return {
+    ok: true,
+    token,
+  };
+}
