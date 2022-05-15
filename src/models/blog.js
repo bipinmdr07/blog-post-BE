e96a3de40364152b3b072e78075adde7cae832fe
Bipin Manandhar <kubipin07@gmail.com>
2022-05-15 20:33:31 +0545

    Update blog model class.

--- src/models/blog.js
+++ src/models/blog.js
@@ -2,55 +2,22 @@ import { Model } from './model';
 import { ulid } from 'ulid';
 
 class Blog extends Model {
-  constructor({
-    blogId = ulid(),
-    title,
-    content,
-    userId,
-    createdAt = new Date().getTime(),
-    updatedAt = new Date().getTime(),
-  }) {
-    super();
-
-    this.blogId = blogId;
-    this.title = title;
-    this.content = content;
-    this.userId = userId;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
+  constructor() {
+    super('blogs');
   }
 
-  pk() {
-    return `BLOG#${this.userId}`;
-  }
+  async create(params) {
+    const blogId = ulid();
 
-  sk() {
-    return `BLOG#${this.blogId}`;
-  }
-
-  fromItem(item) {
-    if (!item) throw new Error('No Item!');
-    return new Blog({
-      blogId: item.blogId,
-      title: item.title,
-      content: item.content,
-      userId: item.userId,
-      createdAt: item.createdAt,
-      updatedAt: item.updatedAt,
+    await super.create({
+      blogId,
+      ...params,
     });
-  }
 
-  toItem() {
-    return {
-      ...this.keys(),
-      blogId: this.blogId,
-      title: this.title,
-      content: this.content,
-      userId: this.userId,
-      createdAt: this.createdAt,
-      updatedAt: this.updatedAt,
-    };
+    return await super.fetchByKey({
+      blogId,
+    });
   }
 }
 
-export default Blog;
+export default new Blog();
