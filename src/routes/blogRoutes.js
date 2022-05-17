c9b487b329a62cf77bd2e7f611aa49e2e5aee49e
Bipin Manandhar <kubipin07@gmail.com>
2022-05-17 21:28:59 +0545

    Integrate policy middle for blog routes.

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -1,16 +1,25 @@
 import { Router } from 'express';
+import * as blogPolicy from '../middlewares/policies/blogPolicy';
 
 import * as blogsController from '../controllers/blogsController';
 
 export const privateRouter = Router();
 export const router = Router();
 
-router.post('/', blogsController.addBlog);
+router.post('/', blogPolicy.createBlog, blogsController.addBlog);
 
 router.get('/', blogsController.getBlogs);
 
-privateRouter.put('/:blogId', blogsController.updateBlog);
+privateRouter.put(
+  '/:blogId',
+  blogPolicy.updateBlog,
+  blogsController.updateBlog
+);
 
-privateRouter.delete('/:blogId', blogsController.deleteBlog);
+privateRouter.delete(
+  '/:blogId',
+  blogPolicy.deleteBlog,
+  blogsController.deleteBlog
+);
 
 // export default router;
